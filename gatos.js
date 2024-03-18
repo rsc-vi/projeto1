4ae65b8785ade77071b4e3a28ca392bbcf6589ff
Caetano <rcs43@aluno.ifal.edu.br>
2024-03-18 08:19:34 -0300

    adiciona o componente Gato

---
+++ gatos.js
@@ -0,0 +1,11 @@
+import{View, Text} from'react-native';
+
+const Gato = () => {
+    return(
+        <View>
+            <Text>My name is re</Text>
+        </View>
+    );
+};
+
+export default Gato;
