f7edaa3b238e3d6fa569c4690eb4a6b941666efd
Caetano <rcs43@aluno.ifal.edu.br>
2024-03-18 09:26:01 -0300

    TEXTA DA IDADE NO SINGULAR E NO PLURAL

--- gatos.js
+++ gatos.js
@@ -1,12 +1,13 @@
 import{View, Text} from'react-native';
 
 const Gato = ({nome, idade}) => {
+    const texto = idade > 1 ? "anos" : "ano"
     return(
         <View>
             <Text>My name is {nome} e eu tenho {idade}</Text>
 
             <Text>Nome: {nome}</Text>
-            <Text>Idade: {idade}</Text>
+            <Text>Idade: {idade} {texto}</Text>
         </View>
     );
 };
